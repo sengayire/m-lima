57492b71f3009ce41b217c9e0c006c5bdc5a78ad
SENGAYIRE <psengayire@gmail.com>
2020-05-02 01:36:55 +0200

    modify my order componets

---
+++ src/components/UserAccount/ShippingAddress.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Form } from 'semantic-ui-react';
+import { Check, Close } from '@material-ui/icons';
+import { FormCard, FormInput, FormButton } from '../commons';
+
+const ShippingAddress = () => (<FormCard header='Personal information' contents={
+            <Form>
+            <div className='form-content'>
+            <div className='form-input-container'>
+            <FormInput icon='caret down' label='Country' placeholder='Rwanda' bordered/>
+            <FormInput icon='caret down' label='Province' bordered/>
+            <FormInput icon='caret down' label='District' bordered/>
+            <FormInput label='Contact name' bordered/>
+            </div>
+            <div className='form-input-container'>
+            <FormInput icon='caret down' label='Sector' bordered/>
+            <FormInput icon='caret down' label='Cell' bordered />
+            <FormInput icon='caret down' label='Village' bordered/>
+            <FormInput label='Phone No.' bordered/>
+            </div>
+             <div>
+
+            </div>
+            </div>
+        <div className='form-bottom-container'>
+            <div className='user-info-container'>
+             <div className='user-info'>
+                 <span>Country: XXXXX</span>
+                 <span>Province: XXXXX</span>
+                 <span>District: XXXXX</span>
+                 <span>Contact name: surname firstName</span>
+             </div>
+             <div className='user-info'>
+                 <span>Sector: XXXXX</span>
+                 <span>Cell: XXXXX</span>
+                 <span>Village: XXXXX </span>
+                 <span>Phone No: XXXXX</span>
+             </div>
+            </div>
+            <div className='form-button-container'>
+               <FormButton icon={<Check />} background='#1e90ff' title='Save changes'/>
+               <FormButton icon={<Close />} background='#a4b0be' title='Deactivate account'/>
+            </div>
+        </div>
+        </Form>
+    }/>);
+
+
+export default ShippingAddress;
--- src/components/commons/FormInput/FormInput.js
+++ src/components/commons/FormInput/FormInput.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Form, Input } from 'semantic-ui-react';
 import './Input.scss';
 
-const FormInput = ({ label, placeholder, icon, bordered, size }) => (
+const FormInput = ({ label, placeholder, icon, bordered, size, type }) => (
         <div>
             <Form.Field className='input-field'>
                 <span>{label}</span>
-                <Input size={size} placeholder={placeholder} icon={icon} className={!bordered ? 'form-input' : 'form-input-bordered'}/>
+                <Input type={type} size={size} placeholder={placeholder} icon={icon} className={!bordered ? 'form-input' : 'form-input-bordered'}/>
             </Form.Field>
         </div>
 );
